31e97b69132d1c5844d8689dba4ea4ce9bf3c0ce
stephen emmahi nartey <emmahiofficial@gmail.com>
2026-04-28 00:09:47 -0400

    modify changes in main.js file

--- apps/frontend/src/scripts/main.js
+++ apps/frontend/src/scripts/main.js
@@ -1,5 +1,3 @@
-import { QUOTES } from "./data/quotes.js";
-import { STATE } from "./state/state.js";
 import { getRandomQuote, renderQuote } from "./features/features.js";
 
 const BTN_QUOTE = document.getElementById("btn-quote");
